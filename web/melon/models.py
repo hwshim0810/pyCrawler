ea4506260c8a22706ec93673a708d4e6ddcf56aa
Shim.hw <shim.hw0810@gmail.com>
2017-04-27 15:23:18 +0900

    Separate model from views

--- web/melon/models.py
+++ web/melon/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, Error
 from django.utils import timezone
 import math
 
@@ -11,23 +11,6 @@ class DailyRank(models.Model):
     albumImg = models.TextField()
     crawledDate = models.DateField(default=timezone.now)
 
-    def get_dailydata(self, page):
-        per_page = 100
-        start_num = (page - 1) * 100
-        end_num = start_num + per_page
-        next_page = page + 1
-
-        total_page = int(math.ceil(float(self.objects.all().count() / per_page)))
-        if total_page == 0:
-            total_page = 1
-
-        if next_page > total_page:
-            next_page = -1
-
-        return {'ranks': self.objects.all().order_by('-crawledDate', 'rank')[start_num:end_num],
-                'next_page': next_page,
-                'total_page': total_page}
-
     def publish(self):
         self.save()
 
@@ -35,3 +18,61 @@ class DailyRank(models.Model):
         return self.title
 
 
+def get_daily(page):
+    per_page = 100
+    start_num = (page - 1) * 100
+    end_num = start_num + per_page
+    next_page = page + 1
+
+    total_page = int(math.ceil(float(DailyRank.objects.all().count() / per_page)))
+    if total_page == 0:
+        total_page = 1
+
+    if next_page > total_page:
+        next_page = -1
+
+    return {'ranks': DailyRank.objects.all().order_by('-crawledDate', 'rank')[start_num:end_num],
+            'next_page': next_page,
+            'total_page': total_page}
+
+
+def parse(html):
+    # 순위
+    rank_spans = html.find_all('span', {'class': "rank"})
+    edited_rank = []
+    for span in rank_spans:
+        edited_rank.append(span.text)
+
+    # 앨범이미지
+    album_imgs = html.find_all('a', {'class': "image_type15"})
+    edited_img = []
+    for link in album_imgs:
+        edited_img.append(link.img.get('src'))
+
+    # 노래제목
+    title_divs = html.find_all('div', {'class': "ellipsis rank01"})
+    edited_title = []
+    for link in title_divs:
+        edited_title.append(link.span.strong.a.text)
+
+    # 가수
+    singer_divs = html.find_all('div', {'class': "ellipsis rank02"})
+    edited_singer = []
+    for link in singer_divs:
+        edited_singer.append(link.a.text)
+
+    # 앨범명
+    album_divs = html.find_all('div', {'class': "ellipsis rank03"})
+    edited_album = []
+    for link in album_divs:
+        edited_album.append(link.a.text)
+
+    for i in range(0, 100):
+        song = DailyRank(
+            rank=int(edited_rank[i]), singer=edited_singer[i],
+            title=edited_title[i], album=edited_album[i], albumImg=edited_img[i])
+        try:
+            song.save()
+            return True
+        except Error:
+            return False
--- web/melon/views.py
+++ web/melon/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .worker.worker import gathering
-from melon.models import DailyRank
+from .models import get_daily, parse
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request, page=1):
     page = int(page)
-    result = DailyRank.get_dailydata(DailyRank, page)
+    result = get_daily(page)
     ranks = result['ranks']
     crawled_date = ''
 
@@ -38,43 +38,11 @@ def is_gather(request):
 @login_required(login_url='/accounts/login/')
 def do_gather(request):
     html = gathering()
+    res = parse(html)
 
-    # 순위
-    rank_spans = html.find_all('span', {'class': "rank"})
-    edited_rank = []
-    for span in rank_spans:
-        edited_rank.append(span.text)
+    if res:
+        return HttpResponseRedirect('/daily/')
+    else:
+        return HttpResponse('DB Exception')
 
-    # 앨범이미지
-    album_imgs = html.find_all('a', {'class': "image_type15"})
-    edited_img = []
-    for link in album_imgs:
-        edited_img.append(link.img.get('src'))
 
-    # 노래제목
-    title_divs = html.find_all('div', {'class': "ellipsis rank01"})
-    edited_title = []
-    for link in title_divs:
-        edited_title.append(link.span.strong.a.text)
-
-    # 가수
-    singer_divs = html.find_all('div', {'class': "ellipsis rank02"})
-    edited_singer = []
-    for link in singer_divs:
-        edited_singer.append(link.a.text)
-
-    # 앨범명
-    album_divs = html.find_all('div', {'class': "ellipsis rank03"})
-    edited_album = []
-    for link in album_divs:
-        edited_album.append(link.a.text)
-
-    for i in range(0, 100):
-        song = DailyRank(rank=int(edited_rank[i]), singer=edited_singer[i], title=edited_title[i],
-                         album=edited_album[i], albumImg=edited_img[i])
-        try:
-            song.save()
-        except Exception:
-            return HttpResponse('DB Exception')
-
-    return HttpResponseRedirect('/daily/')
